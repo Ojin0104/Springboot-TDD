ff1257675746f7aadb31f2105e20c8fd144aaedd
yeongjin <hanyj0104@naver.com>
2023-04-25 02:12:39 +0900

    상품/주문 TDD개발 시작

---
+++ src/main/java/com/productorderservice/poAPI/POApiApplication.java
@@ -0,0 +1,13 @@
+package com.productorderservice.poAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class POApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(POApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/productorderservice/poAPI/POApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.productorderservice.poAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class POApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
